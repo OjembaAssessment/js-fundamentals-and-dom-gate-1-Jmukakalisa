e4ce263fecfbbcfc78bf642fb6f93b05f2623640
Jmukakalisa <j.mukakalis@alustudent.com>
2023-07-24 12:55:09 +0200

    Exercise 2

--- Exercise 2 - Word Cloud/cloud.js
+++ Exercise 2 - Word Cloud/cloud.js
@@ -1,1 +1,11 @@
-console.log("Exercise 2 - Word Cloud");
+// count the how many time each word appeared in the paragraph
+function countWordOccurances(text) {
+    var words = text.toLowerCase().match(/\b\w+\b/g); 
+    var wordCount = {};
+  
+    for (var word of words) {
+      wordCount[word] = (wordCount[word] || 0) + 1;
+    }
+  
+    return wordCount;
+  }
